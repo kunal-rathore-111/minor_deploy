7b285884c0e7708e409983fa8fb25ad1dff626a3
Kunal-Rathore <kunalworkspace111@gmail.com>
2025-11-16 12:02:24 +0530

    trigger rebuild with env vars

--- b/src/index.js
+++ b/src/index.js
@@ -5,14 +5,14 @@ const cors = require("cors");
 
 const app = require("./app");
 const allowedOrigins = [
-    "https://minor-deploy-64gx.vercel.app", // production frontend
-    "http://localhost:5173" // local development
+    "https://minor-deploy-64gx.vercel.app", //  frontend
+    "http://localhost:5173"
 ];
 app.use(cors({
     origin: allowedOrigins,
     credentials: true
 }));
-app.set('trust proxy', 1); // trust first proxy (important for Vercel)
+app.set('trust proxy', 1); // Important for cookies on Vercel
 
 
 const { connectDB } = require("./config/db");
